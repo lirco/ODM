a4955dbd058736d45109117ed0d22b2665ca64e1
lirco <lirco77@gmail.com>
2025-03-20 17:01:46 +0200

    in progress

--- dbt/dbt_project/company_dwh/models/entities/pets/daily/pet_daily.sql
+++ dbt/dbt_project/company_dwh/models/entities/pets/daily/pet_daily.sql
@@ -1,7 +1,8 @@
 select  a.*,
+        dd.date_day,
         b.* exclude (pet_id),
-        c.* exclude (pet_id),
-        d.* exclude (pet_id)
+        c.* exclude (pet_id)
 from    {{ref('pet_dim')}}  a
-left join {{ref('pet_daily__post_submit_agg')}} b on a.pet_id = b.pet_id
-left join {{ref('pet_daily__post_like_agg')}} e on a.pet_id = b.pet_id
+left join   {{ref('pet_dim')}}  dd on dd.date_day <= current_date()
+left join {{ref('pet_daily__post_submit_agg')}} b on a.pet_id = b.pet_id and b.date_day = dd.date_day
+left join {{ref('pet_daily__post_like_agg')}} c on a.pet_id = c.pet_id and c.date_day = dd.date_day
